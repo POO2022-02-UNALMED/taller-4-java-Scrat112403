0f7cd12ce3e597ff81c933bba7a57bb2d1d11a0c
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-04 19:43:47 -0500

    A

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -6,33 +6,36 @@ public class Asignatura {
     String n = nombre;
     int codigoInterno;
     int codigoExterno;
+    Tipo tipo;
 
     Asignatura() {
-        this(0);
+        this("Sin nombre", Tipo.FUNDAMENTACION);
     }
 
-    Asignatura(long codigoInterno) {
-        this("Sin nombre", (int) codigoInterno, 0);
+    Asignatura(Tipo tipo, int codigoInterno) {
+        this("Sin nombre", (int) codigoInterno, 0, tipo);
     }
 
-    Asignatura(int codigoExterno) {
-        this("Sin nombre", 0, codigoExterno);
+    Asignatura(int codigoExterno, Tipo tipo) {
+        this("Sin nombre", 0, codigoExterno, tipo);
     }
 
-    Asignatura(String nombre) {
-        this(nombre, 0, 0);
+    Asignatura(String nombre, Tipo tipo) {
+        this(nombre, 0, 0, tipo);
     }
 
-    Asignatura(String nombre, int codigoInterno, int codigoExterno) {
+    Asignatura(String nombre, int codigoInterno, int codigoExterno, Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
+        this.tipo= tipo;
     }
 
-    void cambiarDatos(int codigoInterno, int codigoExterno, String nombre) {
+    void cambiarDatos(int codigoInterno, int codigoExterno, String nombre, Tipo tipo) {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
+        this.tipo= tipo;
     }
 
     void cambiarDatos(double codigoInterno) {
