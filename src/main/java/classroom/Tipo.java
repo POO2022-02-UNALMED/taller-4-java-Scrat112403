62d2d4f1c076cb1f7d36c8f5e4531746b991b320
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-04 19:17:56 -0500

    T

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,11 +1,11 @@
 package classroom;
 
-private enum Tipo {
+private class Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private int codigo;
 	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	private Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 	}
